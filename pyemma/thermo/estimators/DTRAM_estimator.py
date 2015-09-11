63524cca5701b3dca338c9b7c1bdb2415692cd3e
ppxasjsm <antonia.mey@fu-berlin.de>
2015-09-11 18:17:42 +0200

    [thermo] draft dtram

---
+++ pyemma/thermo/estimators/DTRAM_estimator.py
@@ -0,0 +1,17 @@
+__author__='wehmeyer, mey'
+
+import numpy as _np
+from pyemma._base.estimator import Estimator as _Estimator
+from pyemma.thermo.models.multi_therm import MultiThermModel as _MultiThermModel
+from pyemma.util import types as _types
+
+class DTRAM(_Estimator, _MultiThermModel):
+    r"""I am a wham class"""
+    def __init__(self, b_K_i, stride=1, dt_traj='1 step', maxiter=100000, maxerr=1e-5):
+        raise NotImplementedError("I am not implemented")
+
+    def _estimate(self):
+        raise NotImplementedError("I am not implemented")
+
+    def log_likelihood(self):
+        raise NotImplementedError("I am not implemented")
