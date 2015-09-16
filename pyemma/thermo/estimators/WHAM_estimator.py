f8aa02c324718a42af569a79de1d658b283b788f
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-09-16 16:39:47 +0200

    completing WHAM draft

--- pyemma/thermo/estimators/WHAM_estimator.py
+++ pyemma/thermo/estimators/WHAM_estimator.py
@@ -4,17 +4,39 @@ import numpy as _np
 from six.moves import range
 from pyemma._base.estimator import Estimator as _Estimator
 from pyemma.thermo.models.multi_therm import MultiThermModel as _MultiThermModel
+from pyemma.thermo import StationaryModel as _StationaryModel
 from pyemma.util import types as _types
 from thermotools import wham as _wham
 
 class WHAM(_Estimator, _MultiThermModel):
-    r"""I am a wham class"""
+    """
+    Example
+    -------
+    >>> from pyemma.thermo import WHAM
+    >>> import numpy as np
+    >>> B = np.array([[0, 0],[0.5, 1.0]])
+    >>> wham = WHAM(B)
+    >>> traj1 = np.array([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,1,1,0,0,0]]).T
+    >>> traj2 = np.array([[1,1,1,1,1,1,1,1,1,1],[0,1,0,1,0,1,1,0,0,1]]).T
+    >>> wham.estimate([traj1, traj2])
+    >>> wham.log_likelihood()
+    -9.8058241189353108
+    >>> wham.N_K_i
+    array([[7, 3],
+           [5, 5]], dtype=int32)
+    >>> wham.stationary_distribution
+    array([ 0.38173596,  0.61826404])
+    >>> wham.meval('stationary_distribution')
+    [array([ 0.38173596,  0.61826404]), array([ 0.50445327,  0.49554673])]
+    """
     def __init__(self, b_K_i_full, stride=1, dt_traj='1 step', maxiter=100000, maxerr=1e-5):
         self.b_K_i_full = _types.ensure_ndarray(b_K_i_full, ndim=2, kind='numeric')
         self.stride = stride
         self.dt_traj = dt_traj
         self.maxiter = maxiter
         self.maxerr = maxerr
+        # set derived quantities
+        self.nthermo, self.nstates_full = b_K_i_full.shape
 
     def _estimate(self, trajs):
         """
@@ -34,8 +56,7 @@ class WHAM(_Estimator, _MultiThermModel):
             _types.assert_array(ttraj, ndim=2, kind='i')
             assert _np.shape(ttraj)[1] == 2
         # harvest state counts
-        self.N_K_i_full = _np.zeros(
-            shape=(self.nthermo, self.nstates_full, self.nstates_full), dtype=_np.intc)
+        self.N_K_i_full = _np.zeros(shape=(self.nthermo, self.nstates_full), dtype=_np.intc)
         for ttraj in trajs:
             for K in range(self.nthermo):
                 for i in range(self.nstates_full):
@@ -43,8 +64,8 @@ class WHAM(_Estimator, _MultiThermModel):
                         (ttraj[::self.stride, 0] == K) * (ttraj[::self.stride, 1] == i)).sum()
         # active set
         # TODO: check for active thermodynamic set!
-        self.active_set = _np.where(self.N_K_i_full.sum(axis=0)> 0)
-        self.N_K_i = self.N_K_i_full[:, self.active_set]
+        self.active_set = _np.where(self.N_K_i_full.sum(axis=0) > 0)[0]
+        self.N_K_i = _np.ascontiguousarray(self.N_K_i_full[:, self.active_set])
         log_N_K = _np.log(self.N_K_i.sum(axis=1)).astype(_np.float64)
         log_N_i = _np.log(self.N_K_i.sum(axis=0)).astype(_np.float64)
         self.b_K_i = _np.ascontiguousarray(self.b_K_i_full[:, self.active_set], dtype=_np.float64)
@@ -67,9 +88,14 @@ class WHAM(_Estimator, _MultiThermModel):
             fki = self.b_K_i + fi[_np.newaxis, :] - fk[:, _np.newaxis]
             if _np.linalg.norm(old_fki - fki) < self.maxerr:
                 break
+        # get stationary models
+        sms = [_StationaryModel(
+            pi=_np.exp(fk[K, _np.newaxis] - self.b_K_i[K, :] - fi),
+            f=self.b_K_i[K, :] + fi - fk[K, _np.newaxis],
+            normalize_energy=False, label="K=%d" % K) for K in range(self.nthermo)]
         # set model parameters to self
         # TODO: find out what that even means...
-        self.set_model_params(f_therm=fk, f=fi)
+        self.set_model_params(models=sms, f_therm=fk, f=fi)
         # done, return estimator (+model?)
         return self
 
